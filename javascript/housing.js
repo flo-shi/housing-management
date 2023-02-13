bae5dfd5e11891a3f3ecd8a4a8e9bd46aa4a4a95
flo-shi <kaniaflorence@gmail.com>
2023-02-13 08:55:22 +0300

    initial commit

---
+++ javascript/housing.js
@@ -0,0 +1,18 @@
+$('document').ready(function(){
+
+$('.login').click(function(){
+   window.location.href="./pages/login.html"
+})
+$('.signup').click(function(){
+   window.location.href="./pages/sign-up.html"
+})
+$('#carousel').slick()
+})
+
+/*function Hi(){
+   window.location.href="./pages/login.html";
+}
+var signUpBtn= document.getElementsByClassName("signup")[0];
+signUpBtn.addEventListener('click',function(){
+   window.location.href="./pages/sign-up.html";
+})*/
