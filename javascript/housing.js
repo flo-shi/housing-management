dce96fb555097b56f0152128f6e9277207fc1928
flo-shi <kaniaflorence@gmail.com>
2023-02-19 16:45:54 +0300

    More changes

--- javascript/housing.js
+++ javascript/housing.js
@@ -7,6 +7,10 @@ $('.signup').click(function(){
    window.location.href="./pages/sign-up.html"
 })
 $('#carousel').slick()
+// $('label').click(function(){
+//    $(this).css("color","rgb(236, 27, 97)")
+
+// })
 })
 
 /*function Hi(){
@@ -17,12 +21,12 @@ signUpBtn.addEventListener('click',function(){
    window.location.href="./pages/sign-up.html"; 
 })*/
 
-const label = document.querySelectorAll(".label")
-const labelArray = [...label]
-label.forEach((element) => {
-   element.addEventListener("click" , () => {
-      element.classList.add("active")
-   })
-})
+// const label = document.querySelectorAll(".label")
+// const labelArray = [...label]
+// label.forEach((element) => {
+//    element.addEventListener("click" , () => {
+//       element.classList.add("active")
+//    })
+// })
 
-console.log(labelArray)
+// console.log(labelArray)
