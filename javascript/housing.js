c84459a0f2ec8cfa69a5f842c4e8719085e8f68e
flo-shi <kaniaflorence4@gmail.com>
2023-03-03 10:48:08 +0300

    commit changes login and sign-up

--- javascript/housing.js
+++ javascript/housing.js
@@ -1,3 +1,4 @@
+// jquery
 $('document').ready(function(){
 
 $('.login').click(function(){
@@ -9,24 +10,23 @@ $('.signup').click(function(){
 $('#carousel').slick()
 $('label').click(function(){
    $(this).css("color","rgb(236, 27, 97)")
-
 })
 })
+ 
+// javascript
+function showPassword(){
+   var x = document.getElementById("password");
+   var x = document.getElementById("eye");
+   var x = document.getElementById("eye-slash");
 
-/*function Hi(){
-   window.location.href="./pages/login.html";
+   if( x.type === 'password'){
+      x.type = "text";
+      y.style.display = "block";
+      z.style.display = "none";
+   }
+   else {
+      x.type = "password";
+      y.style.display = "none";
+      z.style.display = "block";
+   }
 }
-var signUpBtn= document.getElementsByClassName("signup")[0];
-signUpBtn.addEventListener('click',function(){
-   window.location.href="./pages/sign-up.html"; 
-})*/
-
-// const label = document.querySelectorAll(".label")
-// const labelArray = [...label]
-// label.forEach((element) => {
-//    element.addEventListener("click" , () => {
-//       element.classList.add("active")
-//    })
-// })
-
-// console.log(labelArray)
