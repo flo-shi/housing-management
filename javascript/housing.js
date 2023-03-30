e95d8fb3dda1a15df9729cae204def2c431f1f42
flo-shi <kaniaflorence4@gmail.com>
2023-03-30 03:41:19 +0300

    changes main page

--- javascript/housing.js
+++ javascript/housing.js
@@ -1,47 +1,50 @@
 // jquery
-$('document').ready(function(){
+$("document").ready(function () {
+  $(".login").click(function () {
+    window.location.href = "./pages/login.html";
+  });
+  $(".signup").click(function () {
+    window.location.href = "./pages/sign-up.html";
+  });
+  $("#carousel").slick();
+  $("label").click(function () {
+    $(this).css("color", "rgb(236, 27, 97)");
+  });
+});
 
-$('.login').click(function(){
-   window.location.href="./pages/login.html"
-})
-$('.signup').click(function(){
-   window.location.href="./pages/sign-up.html"
-})
-$('#carousel').slick()
-$('label').click(function(){
-   $(this).css("color","rgb(236, 27, 97)")
-})
-})
- 
-
+// expand
+var media = document.querySelectorAll(".media");
+function expandContent() {
+  media.classList.add("expand");
+}
+media.addEventListener("click", expandContent());
 
 // Paging
-const pages = document.querySelectorAll('.page');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-console.log(prevButton)
-let currentPage = 0;
+// const pages = document.querySelectorAll(".page");
+// const prevButton = document.querySelector(".prev");
+// const nextButton = document.querySelector(".next");
+// console.log(prevButton);
+// let currentPage = 0;
 
-function showPage(pageIndex) {
-  pages[currentPage].classList.remove('active');
-  pages[pageIndex].classList.add('active');
-  currentPage = pageIndex;
-}
+// function showPage(pageIndex) {
+//   pages[currentPage].classList.remove("active");
+//   pages[pageIndex].classList.add("active");
+//   currentPage = pageIndex;
+// }
 
-function prevPage() {
-  if (currentPage > 0) {
-    showPage(currentPage - 1);
-  }
-}
+// function prevPage() {
+//   if (currentPage > 0) {
+//     showPage(currentPage - 1);
+//   }
+// }
 
-function nextPage() {
-  if (currentPage < pages.length - 1) {
-    showPage(currentPage + 1);
-  }
-}
- 
-prevButton.addEventListener('click', prevPage());
-nextButton.addEventListener('click', nextPage());
+// function nextPage() {
+//   if (currentPage < pages.length - 1) {
+//     showPage(currentPage + 1);
+//   }
+// }
+
+// prevButton.addEventListener("click", prevPage());
+// nextButton.addEventListener("click", nextPage());
 
 // showPage(currentPage);
-
